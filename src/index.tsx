f25e2a1a004e53236c52d9776eb27265094308c6
Nicholas Molnar <nicholas@xmtp.com>
2022-10-31 20:20:52 -0700

    Complete code

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,10 @@
 import React from 'react'
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
-import { Client, ClientOptions, DecodedMessage, Stream } from '@xmtp/xmtp-js'
-import { readFileSync, writeFileSync } from 'fs'
-import { Wallet } from 'ethers'
+import { Client, ClientOptions, SortDirection } from '@xmtp/xmtp-js'
 import { render } from 'ink'
 import { MessageList, MessageStream, Message } from './renderers'
-import { truncateEthAddress } from './utils'
-
-const WALLET_FILE_LOCATION = './xmtp_wallet'
-
-const loadWallet = () => {
-  try {
-    const existing = readFileSync(WALLET_FILE_LOCATION)
-    return Wallet.fromMnemonic(existing.toString())
-  } catch (e) {
-    const newWallet = Wallet.createRandom()
-    writeFileSync(WALLET_FILE_LOCATION, newWallet.mnemonic.phrase)
-    return newWallet
-  }
-}
+import { loadWallet, truncateEthAddress } from './utils'
 
 const getClient = (env: ClientOptions['env']) => {
   const wallet = loadWallet()
@@ -27,45 +12,31 @@ const getClient = (env: ClientOptions['env']) => {
 }
 
 yargs(hideBin(process.argv))
+  .command('init', 'Initialize wallet', {}, async (argv: any) => {
+    const client = await getClient(argv.env)
+    console.log('Your wallet address is', client.address)
+  })
   .command(
     'send <address> <message>',
-    'Send a message',
-    { address: { type: 'string' } },
+    'Send a message to a blockchain address',
+    {
+      address: { type: 'string', demand: true },
+      message: { type: 'string', demand: true },
+    },
     async (argv: any) => {
-      const client = await getClient(argv.env)
-      const conversation = await client.conversations.newConversation(
-        argv.address.toString()
-      )
-      const message = await conversation.send(argv.message)
-      console.log('Message sent')
-      render(<Message {...message} />)
-    }
-  )
-  .command(
-    'stream',
-    'Stream messages',
-    (argv) => argv.option('address', { alias: 'a', type: 'string' }),
-    async (argv: any) => {
-      const client = await getClient(argv.env)
-      let stream: Stream<DecodedMessage> | AsyncGenerator<DecodedMessage>
-      if (argv.address) {
-        const convo = await client.conversations.newConversation(
-          argv.address.toString()
-        )
-        stream = await convo.streamMessages()
-      } else {
-        stream = await client.conversations.streamAllMessages()
-      }
-
-      render(<MessageStream stream={stream} title="Streaming messages" />)
+      const { env, message, address } = argv
+      const client = await Client.create(loadWallet(), { env })
+      const conversation = await client.conversations.newConversation(address)
+      const sent = await conversation.send(message)
+      render(<Message {...sent} />)
     }
   )
   .command(
     'list-messages <address>',
     'List all messages from an address',
-    { address: { type: 'string' } },
-    async (argv: any) => {
-      const client = await getClient(argv.env)
+    { address: { type: 'string', demand: true } },
+    async (argv) => {
+      const client = await getClient(argv.env as ClientOptions['env'])
       const convo = await client.conversations.newConversation(argv.address)
       const messages = await convo.messages()
       render(
@@ -78,12 +49,39 @@ yargs(hideBin(process.argv))
       )
     }
   )
+  .command(
+    'stream',
+    'Stream messages coming from any address',
+    async (argv: any) => {
+      const client = await getClient(argv.env)
+      const stream = await client.conversations.streamAllMessages()
+
+      render(<MessageStream stream={stream} title="Streaming messages" />)
+    }
+  )
+  .command(
+    'stream <address>',
+    'Stream messages from a particular address',
+    { address: { type: 'string', demand: true } },
+    async (argv: any) => {
+      const client = await getClient(argv.env)
+      const convo = await client.conversations.newConversation(argv.address)
+      const stream = await convo.streamMessages()
+
+      render(
+        <MessageStream
+          stream={stream}
+          title={`Streaming messages from ${argv.address}`}
+        />
+      )
+    }
+  )
   .option('env', {
     alias: 'e',
     type: 'string',
     default: 'dev',
-    choices: ['dev', 'production'],
-    description: 'The address you want to communicate with',
+    choices: ['dev', 'production'] as const,
+    description: 'The XMTP environment to use',
   })
   .demandCommand(1)
   .parse()
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,19 @@
+import { readFileSync, writeFileSync } from 'fs'
+import { Wallet } from 'ethers'
+
+const WALLET_FILE_LOCATION = './xmtp_wallet'
+
+export const loadWallet = () => {
+  try {
+    const existing = readFileSync(WALLET_FILE_LOCATION)
+    return Wallet.fromMnemonic(existing.toString())
+  } catch (e) {
+    const newWallet = Wallet.createRandom()
+    writeFileSync(WALLET_FILE_LOCATION, newWallet.mnemonic.phrase)
+    return newWallet
+  }
+}
+
 const truncateRegex = /^(0x[a-zA-Z0-9]{4})[a-zA-Z0-9]+([a-zA-Z0-9]{4})$/
 
 export const truncateEthAddress = (address: string) => {
